6c2479f1a0b1ed6601c7e3b1acd7a075b1100a61
weirdev <weirdev@outlook.com>
2024-04-06 08:58:00 -0400

    RAII for log publisher

--- src/bg.rs
+++ src/bg.rs
@@ -1,6 +1,51 @@
 use std::cell::UnsafeCell;
+use std::mem::swap;
+// switch to tokio::sync::mpsc
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::RwLock;
+use std::thread::JoinHandle;
+
+pub struct LogPublisherHandle<'a, T> {
+    thread_handle: Option<JoinHandle<()>>,
+    common_sender: &'a CommonSender<T>,
+}
+
+pub fn initiate_logging<'a, T, F>(
+    common_sender: &'a CommonSender<T>,
+    mut publisher: F,
+) -> LogPublisherHandle<'a, T>
+where
+    F: FnMut(T) + Send + 'static,
+    T: Send + 'static,
+{
+    let rx = common_sender.set_new_channel();
+
+    let publisher_thread = std::thread::spawn(move || {
+        // This thread will run so long as there is a sender alive.
+        // Since common sender keeps an instance of the sender,
+        // the thread runs until common sender is dropped or
+        // set_new_channel is called again.
+        for data in rx {
+            publisher(data);
+        }
+    });
+
+    LogPublisherHandle {
+        thread_handle: Some(publisher_thread),
+        common_sender,
+    }
+}
+
+impl<'a, T> Drop for LogPublisherHandle<'a, T> {
+    fn drop(&mut self) {
+        // Close the sender so the publisher thread can exit
+        self.common_sender.close_channel();
+        // Wait for the publisher thread to exit
+        let mut thread_handle: Option<JoinHandle<()>> = None;
+        swap(&mut self.thread_handle, &mut thread_handle);
+        thread_handle.unwrap().join().unwrap();
+    }
+}
 
 pub struct CommonSender<T> {
     tx: RwLock<Option<Sender<T>>>,
@@ -10,15 +55,23 @@ unsafe impl<T> Sync for CommonSender<T> {}
 
 impl<T> CommonSender<T> {
     pub const fn new() -> CommonSender<T> {
-        CommonSender { tx: RwLock::new(None) }
+        CommonSender {
+            tx: RwLock::new(None),
+        }
     }
 
     pub fn send(&self, data: T) -> Result<(), ()> {
-        let s = self.tx.read().unwrap().as_ref().map(|s| s.clone()).ok_or(())?;
+        let s = self
+            .tx
+            .read()
+            .unwrap()
+            .as_ref()
+            .map(|s| s.clone())
+            .ok_or(())?;
         s.send(data).map_err(|_| ())
     }
 
-    pub fn set_new_channel(&self) -> Receiver<T> {
+    fn set_new_channel(&self) -> Receiver<T> {
         let (tx, rx) = channel::<T>();
 
         // Must clone the sender to ensure the same sender not used by multiple threads
@@ -28,6 +81,11 @@ impl<T> CommonSender<T> {
         let _ = std::mem::replace(&mut *old_tx_opt, Some(tx));
 
         rx
+    }
+
+    fn close_channel(&self) {
+        let mut old_tx_opt = self.tx.write().unwrap();
+        let _ = std::mem::replace(&mut *old_tx_opt, None);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ mod bg;
 mod test1;
 mod test2;
 mod test3;
-mod test4;
-mod test5;
+// mod test4;
+// mod test5;
+mod test6;
 
 fn main() {
     // test1::main();
     // test2::main();
     // test3::main();
     // test4::main();
-    test5::main();
+    // test5::main();
+    test6::main();
 }
